03cdff747bf95bb8feaccef1aa4d514dd23b08c8
jsolthomsen <jsol.thomsen@gmail.com>
2025-06-08 15:50:34 +0200

    IC

---
+++ GameEngine.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GameEngine.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ GameEngine/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace GameEngine;
+
+public class Class1
+{
+
+}
